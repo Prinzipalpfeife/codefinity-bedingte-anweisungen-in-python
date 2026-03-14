aea071f5595163e5adb918e7f7ef82951644d778
Prinzipalpfeife <>
2026-03-14 21:42:10 +0000

    Herausforderung: Arbeiten mit Logischen Operatoren

--- bedingte_anweisungen_in_python/herausforderung_arbeiten_mit_logischen_operatoren/main.py
+++ bedingte_anweisungen_in_python/herausforderung_arbeiten_mit_logischen_operatoren/main.py
@@ -4,13 +4,13 @@ johns_age = 10
 mikes_age = 18
 
 # Checking if both Bob and John can enter the park (both must be adults)
-can_bob_and_john_enter = bobs_age > 18 or (johns_age > 18)
+can_bob_and_john_enter = (bobs_age >= 18) and (johns_age >= 18)
 
 # Checking if either Alice or Mike can enter the park (at least one must be an adult)
-can_alice_or_mike_enter = alices_age > 18 or (mikes_age >= 18)
+can_alice_or_mike_enter = (alices_age >= 18) or (mikes_age >= 18)
 
 # Checking if Bob, Alice, and John can all enter the park together (all must be adults)
-can_bob_alice_john_enter = bobs_age >= 18 or (alices_age >= 18) or (johns_age >= 18)
+can_bob_alice_john_enter = (bobs_age >= 18) and (alices_age >= 18) and (johns_age >= 18)
 
 # Testing
 print("Can Bob and John enter the park together? ", can_bob_and_john_enter)
