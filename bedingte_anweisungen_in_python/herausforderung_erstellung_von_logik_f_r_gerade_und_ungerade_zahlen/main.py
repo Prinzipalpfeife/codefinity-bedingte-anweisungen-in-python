516fdd096e0477e1d39af1316f4a3adaa51cef03
Prinzipalpfeife <>
2026-03-14 21:46:35 +0000

    Herausforderung: Erstellung von Logik für Gerade und Ungerade Zahlen

---
+++ bedingte_anweisungen_in_python/herausforderung_erstellung_von_logik_f_r_gerade_und_ungerade_zahlen/main.py
@@ -0,0 +1,12 @@
+# Initial variables and values
+num = 8941 % 931
+result = 'blank'
+
+# Determine whether the number is odd or even
+___:
+	___ = ___
+___:
+	___ = ___
+
+# Testing
+print("The number num is", result)
